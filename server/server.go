d8c64735fd23c26497ef937377e0cbab764c14b6
秦翰 <qinhan@xindong.com>
2019-08-09 20:42:47 +0800

    refactor(server): refactor server

--- server/server.go
+++ server/server.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"os/signal"
+
+	"gitlab.com/SausageShoot/admin-server/services/gate/v1"
 	"gitlab.com/SausageShoot/admin-server/utils/logger"
 )
 
+var (
+	port     int
+	logLevel string
+)
+
 func main() {
-	logger.InitLogger("info")
-	logger.Logger.Info("ok")
+	flag.IntVar(&port, "port", 9000, "server port")
+	flag.StringVar(&logLevel, "log-level", "info", "log level, optional( debug | info | warn | error | dpanic | panic | fatal), default is info")
+	flag.Parse()
+
+	// init logger
+	logger.InitLogger(logLevel)
+
+	g := gate.Gate(port)
+
+	// start the server
+	g.Start()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	g.Stop()
 }
--- services/gate/v1/init.go
+++ services/gate/v1/init.go
@@ -1,26 +1,61 @@
 package gate
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"gitlab.com/SausageShoot/admin-server/utils/logger"
 )
 
-type Gate struct {
+type gate struct {
 	port   int
-	engine gin.Engine
+	srv    *http.Server
+	engine *gin.Engine
 }
 
-func (g *Gate) Start() {
-	go func(g *Gate) {
-		if err := g.engine.Run(fmt.Sprintf(":%d", g.port)); err != nil {
-			logger.Logger.Error("start server", logger.ErrorField(err))
+// Gate is the constructor
+func Gate(port int) *gate {
+	e := gin.New()
+	e.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return &gate{
+		port:   port,
+		engine: e,
+	}
+}
+
+// Start begin the gate server
+func (g *gate) Start() {
+	go func(g *gate) {
+		g.srv = &http.Server{
+			Addr:    fmt.Sprintf(":%d", g.port),
+			Handler: g.engine,
+		}
+
+		logger.Logger.Info("Server Start", logger.Field("port", g.port))
+		if err := g.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Logger.Fatal("Server Start", logger.ErrorField(err))
 		}
 	}(g)
 }
 
-func (g *Gate) Stop() {
+// Stop stop gate server gracefully
+func (g *gate) Stop() {
+	// prevent call stop without start
+	if g.srv != nil {
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
+		if err := g.srv.Shutdown(ctx); err != nil {
+			logger.Logger.Fatal("Server Shutdown", logger.ErrorField(err))
+		}
+	}
+	logger.Logger.Info("Server Exiting")
 }
